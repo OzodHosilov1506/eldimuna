ce8d5b4dd39c3650baf0b9fcda3b390b14c76af9
OzodHosilov1506 <coldiron.eltech222@gmail.com>
2022-07-07 20:53:38 +0500

    A ver 0.4

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+echo '<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wedding house "El DiMuNa"</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/normalize.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="jquery-ui-1.13.1/jquery-ui.css">
+    <link rel="stylesheet" href="jquery-ui-1.13.1/jquery-ui.theme.css">
+    <link rel="stylesheet" href="jquery-ui-1.13.1/jquery-ui.structure.css">
+    <link rel="stylesheet" href="slick-1.8.1/slick/slick.scss">
+    <script src="js/main.js"></script>
+    <script src="slick-1.8.1/slick/slick.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="jquery-ui-1.13.1/jquery-ui.js"></script>
+    <script src="jquery-ui-1.13.1/external/jquery/jquery.js"></script>
+</head>
+<body>
+
+<section class="header">
+    <div class="header-content">
+        <a href="#"><h3 class="brand-name"> "El DiMuNa" </h3></a>
+    </div>
+    <div class="header-center">
+        <img src="img/image1.png" alt="">
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contacts</a></li>
+        </ul>
+    </div>
+</section>
+
+
+
+
+</body>
+
+</html>'
+    ?>
