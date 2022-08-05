89b1988fd3251f8afc95c0ef8a22e8b73f6eb683
OzodHosilov1506 <coldiron.eltech222@gmail.com>
2022-08-05 19:01:31 +0500

    ready Surface

--- js/app.js
+++ js/app.js
@@ -2,19 +2,19 @@
 $(document).ready(function(){
   $('.slider').slick({
     dots: true,
-    infinite: true,
+    infinite: false,
     centerMode: true,
     slidesToShow: 1,
     slidesToScroll: 3,
     speed: 500,
     fade: true,
     cssEase: 'linear',
-    nextArrow: "<button type='button' class='slick-btn slick-next'>next</button>",
-    prevArrow: "<button type='button' class='slick-btn slick-prev'>prev</button>"
+    nextArrow: '<button type="button" class="slick-btn slick-angle slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-angle slick-prev"><</button>'
   });
 });
 
-// $(ducument).slick({
-// })
+
+
 
 
